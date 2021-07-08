5d36f130bc9a0baac830b00da292b06ede649bcb
Kshitij Sharma <sharmakshitij250@gmail.com>
2021-07-08 13:44:05 +0530

    Fix Logout And Change Navbar

--- website/auth.py
+++ website/auth.py
@@ -23,12 +23,12 @@ def login():
         else:
             flash('User does not exist',category='error')
 
-    return render_template('login.html')
+    return render_template('login.html',user=current_user)
 
 @auth.route('/logout')
 @login_required
 def logout():
-    logout_user
+    logout_user()
     return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=['GET','POST'])
@@ -56,4 +56,4 @@ def sign_up():
             flash('Account created!',category='success')
             login_user(user,remember=True)
             return redirect(url_for('views.home'))
-    return render_template('sign_up.html')
+    return render_template('sign_up.html',user=current_user)
